a561d340f0ff85e9bd586bfd3c9a210ffbfd28c7
Miguel Brown <miguel.a.brown@gmail.com>
2023-11-30 20:47:25 +0000

    :hammer: refactored reformat code for dwh input

--- scripts/fusion_standardization_wrapper.R
+++ scripts/fusion_standardization_wrapper.R
@@ -2,6 +2,7 @@
 library(optparse)
 library(readr)
 library(annoFuse)
+library(dplyr)
 
 
 option_list <- list(
@@ -43,17 +44,24 @@ output_basename <- opts$output_basename
 fusion_df<-read_tsv(fusions_tsv)
 if (caller == "DGD" ){
     # all required for annoFuse downstream
-    placeholder <- c("Gene2A", "Gene2B", "LeftBreakpoint", "RightBreakpoint")
+    ## rename existing
+    colnames(fusion_df)[colnames(fusion_df) %in% c("kf_biospecimen_id", "5_prime_gene", "3_prime_gene")] <- c("Sample", "Gene1A", "Gene1B")
+    ## create fusion names
+    fusion_df[, "FusionName"] <- paste(fusion_df$Gene1A, fusion_df$Gene1B, sep="--")
+    ## Fillers
+    placeholder <- c("Gene2A", "Gene2B", "LeftBreakpoint", "RightBreakpoint", "annots")
     fusion_df[ , placeholder] <- ""
+    fusion_df[, "Fusion_Type"] <- "Not available"
     # cols required for fusion qc filtering
     ct_cols <- c('JunctionReadCount', 'SpanningFragCount')
     fusion_df[ , ct_cols] <- 0
-    # annots needs to go here, as last blank column gets filled by fusion annotator
-    fusion_df[, "annots"] <- ""
-
     fusion_std_df<-fusion_standardization(fusion_df, caller="CUSTOM")
+    # reorg columns for better matching with old format
+    fusion_std_df <- fusion_std_df %>% select(Sample, FusionName, Fusion_Type, Gene1A, Gene1B, Gene2A, Gene2B, LeftBreakpoint, RightBreakpoint, JunctionReadCount, SpanningFragCount, `5_prime_transcript`, `5_prime_region`, `3_prime_transcript`, `3_prime_region`, variant_tier, annots)
 } else{
     fusion_std_df<-fusion_standardization(fusion_df, caller=caller, tumorID=tumorID, input_json_file=input_json_file)
 }
-write.table(fusion_std_df, paste(output_basename, "_standardized.tsv", sep=""), sep="\t",quote=FALSE,row.names = FALSE)
+# output using a hack from https://stackoverflow.com/questions/16430845/removing-backticks-in-r-output
+report <- capture.output(write.table(fusion_std_df, sep="\t",quote=FALSE,row.names = FALSE))
+cat(gsub("`", "", report), sep = "\n", file = paste(output_basename, "_standardized.tsv", sep=""))
 
